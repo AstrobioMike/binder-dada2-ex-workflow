8e4c23c207d62b89278dfe212f0bbec687105476
AstrobioMike <michael.lee0517@gmail.com>
2019-04-29 14:53:14 -0700

    mod

--- install.R
+++ install.R
@@ -4,13 +4,12 @@ install.packages("tidyr")
 install.packages("viridis")
 
 source("https://bioconductor.org/biocLite.R")
-BiocManager::install("phyloseq")
-BiocManager::install("DESeq2")
 biocLite(suppressUpdates = FALSE)
+biocLite("phyloseq")
+biocLite("DESeq2")
 biocLite("ShortRead", suppressUpdates = FALSE)
 
 install.packages("devtools")
-devtools::install_github("tidyverse/ggplot2")
-
 library("devtools")
+devtools::install_github("tidyverse/ggplot2")
 devtools::install_github("benjjneb/dada2")
