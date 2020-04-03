9216ce2e880cc19d6484c77cf957b03c074339ba
AstrobioMike <michael.lee0517@gmail.com>
2020-04-02 17:36:06 -0700

    still trying to get deseq2 to install

--- install.R
+++ install.R
@@ -4,18 +4,16 @@ install.packages("dendextend")
 install.packages("viridis")
 install.packages("reshape")
 install.packages("locfit")
+install.packages("devtools")
+
+devtools::install_github("benjjneb/dada2")
+devtools::install_github("benjjneb/decontam")
+
+devtools::install_version("mvtnorm", version = "1.0-8", repos = "http://cran.us.r-project.org")
+devtools::install_version("fpc", version = "2.1-11.1", repos = "http://cran.us.r-project.org")
 
 source("https://bioconductor.org/biocLite.R")
 biocLite("phyloseq", suppressUpdates = TRUE)
 biocLite("DESeq2", suppressUpdates = TRUE)
 biocLite("ShortRead", suppressUpdates = TRUE)
-
-
-install.packages("devtools")
-devtools::install_github("benjjneb/dada2")
-devtools::install_github("benjjneb/decontam")
-
-
-devtools::install_version("mvtnorm", version = "1.0-8", repos = "http://cran.us.r-project.org")
-devtools::install_version("fpc", version = "2.1-11.1", repos = "http://cran.us.r-project.org")
 biocLite("dendextend", suppressUpdates = TRUE)
